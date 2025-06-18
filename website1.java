88c11c21ba77dba8b2da101752828658e124daf3
alizzyfatima <maliksami200283@gmail.com>
2025-06-18 15:22:28 +0200

    Create website1.java

---
+++ website1.java
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
+ <!-- Font Awesome -->
+ <script src="https://kit.fontawesome.com/996973c893.js" crossorigin="anonymous"></script>
+
+ <!-- Google Fonts -->
+ <link href="https://fonts.googleapis.com/css2?family=Baloo+Tamma+2:wght@400;500;600;700;800&family=Roboto+Slab:wght@200;400;600;900&display=swap" rel="styles
+<!-- Bootstrap CSS -->
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
+
+ <!-- Custom CSS -->
+ <link rel="stylesheet" href="style.css" />
+Mehjabin Shejuti
+20:54
+<!-- Font Awesome -->
+ <script src="https://kit.fontawesome.com/996973c893.js" crossorigin="anonymous"></script>
+
+ <!-- Google Fonts -->
+ <link href="https://fonts.googleapis.com/css2?family=Baloo+Tamma+2:wght@400;500;600;700;800&family=Roboto+Slab:wght@200;400;600;900&display=swap" rel="stylesheet" />
+ <style>
+   .navigation{
+    background: rgb(68, 66, 66);
+    padding: 15px 0;
+   }
+   .navigation a {
+    font-weight: 600;
+    text-decoration: none;
+   }
+    body{
+        margin: 0;
+        font-family: cursive;
+        color: rgb(93, 84, 84);
+    }
+    .main-container{
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        text-align: center;
+        background-image: url(https://i.pinimg.com/originals/70/dc/d2/70dcd293e6e6fd578268570b57e8dab2.jpg);
+        background-attachment: fixed;
+        background-size: cover;
+    }
+    h1{
+        font-size: 20px;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        color: rgb(68, 66, 66);
+    }
+    h2{
+        margin-top: 10px;
+        font-size: 20px;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        color: rgb(68, 66, 66);
+    }
+ </style>
+</head>
+
+<body>
+    <div class="container-fluid navigation">
+        <div class="row">
+        <div class="col-md-6 d-flex justify-content-end align-iteams-center pl-4">
+        <i class="fas fa-code fa-2x text warning pb-1"></i>
+        <h5 class="d-inlinempl-3 text-white mb-0">style</h5>
+        <div class="col-md-6 d-flex justify-content-end align-iteams-center pr4">
+        <div class="rolw w-100 text-center">
+        <div class="col">
+        <a href="#material" class="text-white navigation link"> material</a>
+        <div class="col"> 
+        <a href="#colour" class="text-white navigation link">colour</a>
+        <div class="col"> 
+        <a href="#size" class="text-white navigation-link">size</a>
+        <div class="col"> 
+    </div>
+    </div>
+    </div>
+    </div>
+    <div class="main-container">
+        <h2> ALIZY</h2>
+        <br>
+       <h1>Fashion is a Must</h1>
+    </div>
+
+</div>
+
+    
+</body>
+
+</html>
